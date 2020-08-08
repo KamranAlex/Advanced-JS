e8361e8089ffdbbdeb93bc63f7da73e328f261e9
kamruzzamanK <officialkamran48@gmail.com>
2020-08-08 16:53:25 +0600

    double VS triple Equels

---
+++ double_VS_triple-EQUELS.js
@@ -0,0 +1,16 @@
+const num1 = 2;
+const num2 = "2";
+
+if (num1 == num2) {
+  console.log("Condition True");
+} else {
+  console.log("Condition False");
+}
+
+const first = 2;
+const second = "2";
+if (first === second) {
+  /*checking type of data*/ console.log("Condition True");
+} else {
+  console.log("Condition False");
+}
